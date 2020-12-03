b3a89134d3d0393d23142945145b33cb7fa5ae11
Aman <55349238+X-genius@users.noreply.github.com>
2020-12-03 16:40:35 +0530

    Adding files of whiteBoard integration

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const httpserver=require("http").createServer(app);
+app.use(express.static("activity"));
+const socketServer=require("socket.io")(httpserver);
+socketServer.on("connection",function(socket){
+    console.log("New Client Connected");
+    console.log(socket.id);
+    socket.on("colorChange",function(color){
+        console.log(color);
+        socket.broadcast.emit('rColorChange',color);
+    })
+    socket.on("md",function(point){
+        socket.broadcast.emit('onmd',point);
+    })
+    socket.on("mmo",function(point){
+        socket.broadcast.emit('onmm',point);
+    })
+})
+let port = process.env.PORT||3000;
+httpserver.listen(port,function(){
+    console.log("Server is listening to request at port ",port);
+})
